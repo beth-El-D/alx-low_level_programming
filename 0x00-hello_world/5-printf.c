5902e49e2a6c5bc693be99b14cbd833149c433a9
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-06-15 13:45:01 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - A progthat prints a line using printf f
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
- 	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
