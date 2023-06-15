050279668894acd285b07adc38fb4a2eec0bf876
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-06-15 13:53:34 -0700

    vbn

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,18 @@
 #include stdio.h;
 /**
  * main - A program that prints the size of diffrent computer types
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
-{       
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float f;
+{	 char a;
+	 long int c;
+         long long int d;
+	 float f;
         
-pritf("Size of a char: %1u byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of a char: %1u byte(s)\n",(unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
-pritf("Size of a long int: %1u byte(s)\n",(unsigned long)sizeof(c));
-pritf("Size of a long long int: %1u byte(s)\n",(unsigned long)sizeof(d));
-pritf("Size of a float: %1u byte(s)\n",(unsigned long)sizeof(f));
+printf("Size of a long int: %1u byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a long long int: %1u byte(s)\n",(unsigned long)sizeof(d));
+printf("Size of a float: %1u byte(s)\n",(unsigned long)sizeof(f));
 return (0);
 }
