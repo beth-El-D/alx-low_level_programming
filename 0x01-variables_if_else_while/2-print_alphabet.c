d6237d62f8a8bfe69f3f8f0f766fdd4fce7aef10
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-06-16 10:15:43 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints the alphabet.
  *
- * Return: 0(Success)
+ * Return: 0 (success)
  */
 int main(void)
 {
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 0; i < 26; i++)
 	{
-		putschar(alp[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
