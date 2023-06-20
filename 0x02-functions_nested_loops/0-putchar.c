4834955539e1f31f2b6d1e1460b5bcfe5459146e
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-06-20 03:46:15 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include main.h
+/**
+ * main - printing _putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchat('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
