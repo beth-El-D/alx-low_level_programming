03eb4364b7330c3c46c3f3b257204276c13ed6f3
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-06-20 04:58:29 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x110 - make alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+
+			putchar ('\n');
+			i++;
+		}
+	}
+}
