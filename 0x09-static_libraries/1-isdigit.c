3027d20be10602d0aef8222128a561c2960ef31e
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-07-11 07:15:09 -0700

    Updated

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 throuh 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, o otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Readme file for the last project in alx low level programming.
