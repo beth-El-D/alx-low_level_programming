d9f5dbb649bbdb477c60ffb34c572e664746322e
Bethel Teshome <elbetelteshome2023@gmail.com>
2023-08-07 04:13:45 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ **_strdup - copies a string to a pointer
+ * @str: pointer to a string
+ * Return: a char pointer, NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	i = 0, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	cpy = malloc((sizeof(char) * i) + 1);
+
+	if (cpy == NULL)
+		return (NULL);
+
+	while (j < i)
+	{
+		cpy[j] = str[j];
+	}
+	cpy[j] = '\0';
+	return (cpy);
+}
